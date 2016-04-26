c93880e862837d8084bdd92b339b2168f5a6014b
nbingo <nringach1@hwemail.com>
2016-04-26 15:39:44 -0700

    Made code a lot simpler and more concise.

--- LZW Compression/src/LZWCompressor.java
+++ LZW Compression/src/LZWCompressor.java
@@ -7,28 +7,26 @@ public class LZWCompressor {
 	{
 		BufferedReader reader = new BufferedReader(new FileReader(infilename));
 		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outfilename)));
-		ArrayList<TableEntry> table = new ArrayList<TableEntry>(500);
+		ArrayList<String> table = new ArrayList<String>(500);
 		
 		for (int i = 0; i <= 255; i++)
-			table.add(new TableEntry(i, ""+(char)i));
-		int counter = 256;
+			table.add(""+(char)i);
 		
 		String str = ""+(char)reader.read();
 		
 		while(reader.ready())
 		{
 			char character = (char)reader.read();
-			if (table.contains(new TableEntry(0, str+character)))
+			if (table.contains(str+character))
 				str += character;
 			else
 			{
-				writer.print(table.indexOf(new TableEntry(0, str)));
-				table.add(new TableEntry(counter, str+character));
-				counter++;
+				writer.print(table.indexOf(str));
+				table.add(str+character);
 				str = ""+character;
 			}
 		}
-		writer.print(table.indexOf(new TableEntry(0, str)));
+		writer.print(table.indexOf(str));
 		
 		reader.close();
 		writer.close();
