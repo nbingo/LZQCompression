12228bae1260848b0a1caed53857e60b8bcfd395
nbingo <nringach1@hwemail.com>
2016-04-27 14:29:33 -0700

    Can decompress files now too!

--- LZW Compression/src/LZWDecompressor.java
+++ LZW Compression/src/LZWDecompressor.java
@@ -1,13 +1,41 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
 
 public class LZWDecompressor {
 
 	public static void decompress(String infilename, String outfilename) throws FileNotFoundException, IOException
 	{
-		BufferedReader reader = new BufferedReader(new FileReader (infilename));
+		Scanner reader = new Scanner(new BufferedReader(new FileReader (infilename)));
 		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outfilename)));
 		
+		Hashtable<Integer, String> table = new Hashtable<Integer, String>(500);
 		
+		for (int i = 0; i <= 255; i++)
+			table.put(i,""+(char)i);
+		int counter = 256;
+		
+		int code = reader.nextInt();
+		
+		String str = table.get(code);
+		writer.print(str);
+		
+		String entry = "";
+		
+		while(reader.hasNextInt())
+		{
+			code = reader.nextInt();
+			if (!table.containsKey(code))
+				entry = str + str.charAt(0);
+			else
+				entry = table.get(code);
+			writer.print(entry);
+			table.put(counter, str+entry.charAt(0));
+			counter++;
+			str = entry;
+		}
+		
+		reader.close();
+		writer.close();
 	}
 }
