b8ea343e48e330106ae90d86981f5c60798c5af8
nbingo <nringach1@hwemail.com>
2016-05-02 14:53:07 -0700

    Just a copy since the real one will be binary now. Want to keep text too

---
+++ LZW Compression/src/TextLZWDecompressor.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class TextLZWDecompressor {
+
+	public static void decompress(String infilename, String outfilename) throws FileNotFoundException, IOException
+	{
+		Scanner reader = new Scanner(new BufferedReader(new FileReader (infilename)));
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outfilename)));
+		
+		Hashtable<Integer, String> table = new Hashtable<Integer, String>(500);
+		
+		for (int i = 0; i <= 255; i++)
+			table.put(i,""+(char)i);
+		int counter = 256;
+		
+		int code = reader.nextInt();
+		
+		String str = table.get(code);
+		writer.print(str);
+		
+		String entry = "";
+		
+		while(reader.hasNextInt())
+		{
+			code = reader.nextInt();
+			if (!table.containsKey(code))
+				entry = str + str.charAt(0);
+			else
+				entry = table.get(code);
+			writer.print(entry);
+			table.put(counter++, str+entry.charAt(0));
+			str = entry;
+		}
+		
+		reader.close();
+		writer.close();
+	}
+}
