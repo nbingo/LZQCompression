5f68111544772c4628309a2b28459bf9f3cfc1b1
nbingo <nringach1@hwemail.com>
2016-05-02 15:20:06 -0700

    It will work eventually...

--- LZW Compression/src/LZWDecompressor.java
+++ LZW Compression/src/LZWDecompressor.java
@@ -6,8 +6,10 @@ public class LZWDecompressor {
 
 	public static void decompress(String infilename, String outfilename) throws FileNotFoundException, IOException
 	{
-		Scanner reader = new Scanner(new BufferedReader(new FileReader (infilename)));
+		BufferedReader reader = new BufferedReader(new FileReader (infilename));
 		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outfilename)));
+		
+		int byteLimit = Integer.parseInt(reader.read() + "" + reader.read());
 		
 		Hashtable<Integer, String> table = new Hashtable<Integer, String>(500);
 		
@@ -15,26 +17,39 @@ public class LZWDecompressor {
 			table.put(i,""+(char)i);
 		int counter = 256;
 		
-		int code = reader.nextInt();
+		StringBuffer binary = new StringBuffer();
+		while (reader.ready())
+			binary.append(Integer.toBinaryString(reader.read()));
+		
+		StringBuffer nums = new StringBuffer();
+		for (int i = 0; i < nums.length()/8; i++)
+			nums.append(Integer.parseInt(nums.substring(8*i, (i+1)*8), 2) + " ");
+		
+		Scanner scan = new Scanner(nums.toString());
+		
+		int code = scan.nextInt();
 		
 		String str = table.get(code);
 		writer.print(str);
 		
 		String entry = "";
+		double tableLimit = Math.pow(2, byteLimit);
 		
-		while(reader.hasNextInt())
+		while(scan.hasNextInt())
 		{
-			code = reader.nextInt();
+			code = scan.nextInt();
 			if (!table.containsKey(code))
 				entry = str + str.charAt(0);
 			else
 				entry = table.get(code);
 			writer.print(entry);
-			table.put(counter++, str+entry.charAt(0));
+			if(table.size() < tableLimit)
+				table.put(counter++, str+entry.charAt(0));
 			str = entry;
 		}
 		
 		reader.close();
 		writer.close();
+		scan.close();
 	}
 }
